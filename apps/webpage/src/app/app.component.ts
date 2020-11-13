a806eb4283a4a7d96a0ec93bd4e5825b55db90c1
MSakamaki <mail@m-sakamaki.jp>
2020-11-13 11:29:22 +0900

    add pwa and onesignal

--- apps/webpage/src/app/app.component.ts
+++ apps/webpage/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { OneSignalService } from 'ngx-onesignal';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'onesignal-example-angular-root',
@@ -6,5 +8,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'webpage';
+  private readonly tagsSubscribe$ = new BehaviorSubject<{
+    [key: string]: string;
+  }>({});
+  public readonly tags$ = this.tagsSubscribe$.asObservable();
+
+  constructor(public readonly onesignal: OneSignalService) {
+    (window as any).ngxOnesignal = this.onesignal;
+  }
+
+  onSubscribe() {
+    this.onesignal.subscribe();
+  }
+
+  onUnSubscribe() {
+    this.onesignal.unsubscribe();
+  }
+
+  testPush() {
+    this.onesignal.push([
+      'sendTag',
+      'random',
+      Math.random(),
+      tagsSent => {
+        console.log('tagsSent', tagsSent);
+      },
+    ]);
+  }
+
+  getTags() {
+    this.onesignal.push([
+      'getTags',
+      tags => {
+        this.tagsSubscribe$.next(tags);
+      },
+    ]);
+  }
 }
--- apps/webpage/src/app/app.module.ts
+++ apps/webpage/src/app/app.module.ts
@@ -2,10 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { NgxOneSignalModule } from 'ngx-onesignal';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    ServiceWorkerModule.register('OneSignalSDKWorker.js', {
+      enabled: environment.production,
+    }),
+    NgxOneSignalModule.forRoot({
+      appId: '94953775-34ee-4db9-b4cb-579502c7d6b5',
+      safari_web_id: 'web.onesignal.auto.0f4ef0fc-d020-4184-9066-253532c318fc',
+      notifyButton: {
+        enable: true,
+      },
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
